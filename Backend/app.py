bcdb31bd9159819a6c3259ee69dd8174cd887dd6
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-07-09 13:15:06 -0700

    connect to database

--- Backend/app.py
+++ Backend/app.py
@@ -15,11 +15,29 @@ Base.prepare(autoload_with=engine)
 
 inspector = inspect(engine)
 print(inspector.get_table_names())
+
+columnNames = []
 columns = inspector.get_columns("fire_data")
 for c in columns:
     print(c["name"], c["type"])
+    columnNames.append("wildfire." + c["name"])
+print(columnNames)
 
 wildfires = Base.classes.fire_data
 
 
-app = Flask(__name__)
+"""app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return(
+        f"Available Routes:<br/>"
+    )
+
+@app.route("/api/cawildfires17-20")
+def cawildfires():
+    session = Session(engine)
+
+    
+    wildfireData = session.query(columnNames).all()
+    """
