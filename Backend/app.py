b2a38f265848d117022305c401299da7907879a1
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-07-09 16:43:38 -0700

    update api connection

--- Backend/app.py
+++ Backend/app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect, MetaData, Table, select
 from flask import Flask, jsonify
+from datetime import datetime
 
 cwd = os.getcwd()
 print(cwd)
@@ -24,6 +25,7 @@ print(columnNames)
 
 wildfires = Table("fire_data", metadata, autoload=True, autoload_with=engine)
 #print(wildfires)
+print(engine.execute("select DISCOVERY_DATE from fire_data").first())
 
 """" DOES NOT WORK FOR SOME REASON
 Base = automap_base()
@@ -43,7 +45,11 @@ def home():
 @app.route("/api/cawildfires17-20")
 def cawildfires():
     session = Session(engine)
-    sel = select([wildfires.c.COUNTY, wildfires.c.LATITUDE, wildfires.c.LONGITUDE, wildfires.c.FIRE_NAME, wildfires.c.FIRE_SIZE, wildfires.c.FIRE_SIZE_CLASS, wildfires.c.FIRE_YEAR,  wildfires.c.CAUSE_CLASSIFICATION, wildfires.c.CAUSE])
+    sel = select([wildfires.c.COUNTY, wildfires.c.LATITUDE, wildfires.c.LONGITUDE, 
+                  wildfires.c.FIRE_NAME, wildfires.c.FIRE_SIZE, wildfires.c.FIRE_SIZE_CLASS, wildfires.c.FIRE_YEAR, 
+                  func.strftime("%Y-%m-%d", wildfires.c.DISCOVERY_DATE).label("formatted_date"), 
+                  func.strftime("%Y-%m-%d", wildfires.c.CONTAIN_DATE).label("formatted_date2"), 
+                  wildfires.c.CAUSE_CLASSIFICATION, wildfires.c.CAUSE])
     wildfireData = session.execute(sel).fetchall()
     session.close()
     api = []
@@ -56,8 +62,8 @@ def cawildfires():
         dict["FIRE_SIZE"] = row.FIRE_SIZE
         dict["FIRE_SIZE_CLASS"] = row.FIRE_SIZE_CLASS
         dict["FIRE_YEAR"] = row.FIRE_YEAR
-        #dict["DISCOVERY_DATE"] = row.DISCOVERY_DATE
-        #dict["CONTAIN_DATE"] = row.CONTAIN_DATE
+        dict["DISCOVERY_DATE"] = row.formatted_date
+        dict["CONTAIN_DATE"] = row.formatted_date2
         dict["CAUSE_CLASSIFICATION"] = row.CAUSE_CLASSIFICATION
         dict["CAUSE"] = row.CAUSE
         api.append(dict)
