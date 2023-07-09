bd43a4143d125942ee3f6b83b9715a76b226acad
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-07-08 20:22:29 -0700

    setup database

--- Backend/app.py
+++ Backend/app.py
@@ -5,3 +5,21 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
+
+cwd = os.getcwd()
+print(cwd)
+
+engine = create_engine("sqlite:///Backend/WildfiresDB.db")
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+inspector = inspect(engine)
+print(inspector.get_table_names())
+columns = inspector.get_columns("fire_data")
+for c in columns:
+    print(c["name"], c["type"])
+
+wildfires = Base.classes.fire_data
+
+
+app = Flask(__name__)
