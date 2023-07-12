7584d23813d9821b404fd012220fe856592d949d
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-07-11 21:30:53 -0700

    added cluster map and heat map

--- Frontend/static/js/plots.js
+++ Frontend/static/js/plots.js
@@ -2,12 +2,41 @@ let wildfireApi = "http://127.0.0.1:5000/api/cawildfires17-20"
 
 //console.log(new Date(Date.parse("2017-10-13")).toLocaleString())
 
+let wildfireNumbers = L.map("wildfireNumbersPlot", {
+    center: [37, -119.42],
+    zoom: 5.5, 
+    maxZoom: 30
+});
+
+let wildfireHeatMap = L.map("wildfireHeatMaps", {
+    center: [37, -119.42],
+    zoom: 5.5,
+    maxZoom: 30
+})
+
+function tile(map){
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+tile(wildfireNumbers)
+tile(wildfireHeatMap)
+/*
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(wildfireNumbers);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(wildfireHeatMap);*/
+
 d3.json(wildfireApi).then(function(data){
     let newData = [];
     let markers = L.markerClusterGroup();
+    let heatArray = []
     for(let i = 0; i < data.length; i++) {
         //convert dates
         let oldRecord = data[i];
+        //1st plot
         //console.log(oldRecord["DISCOVERY_DATE"].slice(0, 4) - oldRecord["DISCOVERY_DATE"].slice(4, 6)-oldRecord["DISCOVERY_DATE"].slice(6, 8))
         oldRecord["DISCOVERY_DATE_FORMATTED"] = new Date(Date.parse(`${oldRecord["DISCOVERY_DATE"].slice(0, 4)}- ${oldRecord["DISCOVERY_DATE"].slice(4, 6)}-${oldRecord["DISCOVERY_DATE"].slice(6, 8)}`))
         oldRecord["CONTAIN_DATE_FORMATTED"] = new Date(Date.parse(`${oldRecord["CONTAIN_DATE"].slice(0, 4)}- ${oldRecord["CONTAIN_DATE"].slice(4, 6)}-${oldRecord["CONTAIN_DATE"].slice(6, 8)}`))
@@ -16,23 +45,23 @@ d3.json(wildfireApi).then(function(data){
         //markers
         let lon = data[i].LONGITUDE;
         let lat = data[i].LATITUDE;
-        console.log(lat, lon, data[i].FIRE_NAME)
+        //console.log(lat, lon, data[i].FIRE_NAME)
         markers.addLayer(L.marker([lat, lon]).bindPopup(data[i].FIRE_NAME));
-        //markers.addLayer(L.marker( [Number(lat), Number(lon)]));
+
+        //2nd plot
+        heatArray.push([data[i].LATITUDE, data[i].LONGITUDE, data[i].FREQUENCY]);
     }
-    console.log(data)
+    //console.log(data)
     //console.log(newData)
-    
     wildfireNumbers.addLayer(markers);
+    L.heatLayer(heatArray, {
+        radius: 5,
+        blur: 1,
+        //gradient: { 0.1: 'blue', 0.2: 'lime', 0.5: 'red' },
+        minOpacity: 0.25
+      }).addTo(wildfireHeatMap);
 });
 
-let wildfireNumbers = L.map("map", {
-    center: [32.78, -117.42],
-    zoom: 5.5
-});
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(wildfireNumbers);
   
 
