32bc7b86fc882d21be7102be1071b5d467263d06
Miguel Garcia <miggarcia@csumb.edu>
2023-07-14 15:22:59 -0700

    Javascript updated

--- Frontend/static/js/plots.js
+++ Frontend/static/js/plots.js
@@ -1,21 +1,20 @@
-import pandas from 'pandas-js';
-import plotly from 'plotly.js-dist';
+const os = require('os');
+const { createEngine } = require('sqlalchemy');
+const pandas = require('pandas-js');
+const plotly = require('plotly.js-dist');
 
-// Get the current working directory
-const cwd = process.cwd();
+const cwd = os.cwd();
 console.log(cwd);
 
-// Read the data from the "WildfiresDB" database file
-const wildfires_data = pandas.read_sql_table("fire_data", engine);
+const engine = createEngine('sqlite://Backend/WildfiresDB.db');
 
-// Calculate the average number of wildfires per county
+const wildfires_data = pandas.read_sql_table('fire_data', engine);
+
 const avg_wildfires_per_county = wildfires_data.groupby('COUNTY').size().mean();
-console.log("Average wildfires per county:", avg_wildfires_per_county);
+console.log('Average wildfires per county:', avg_wildfires_per_county);
 
-// Sort the data by county in alphabetical order
 const most_severe_wildfires = wildfires_data[wildfires_data['FIRE_SIZE_CLASS'] === 'G'].sort_values('COUNTY');
 
-// Create a bar graph for the most severe wildfires (Fire Size Class 'G') and average acres burned
 const data = [
   {
     x: most_severe_wildfires['COUNTY'],
@@ -34,7 +33,5 @@ const layout = {
 };
 plotly.newPlot('chart', data, layout);
 
-// Calculate the average acres burned for severe wildfires
 const avg_acres_burned = most_severe_wildfires['FIRE_SIZE'].mean();
-console.log("Average acres burned for severe wildfires:", avg_acres_burned);
-
+console.log('Average acres burned for severe wildfires:', avg_acres_burned);
