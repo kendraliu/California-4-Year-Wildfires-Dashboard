25d6315a8bab11b0073b5dd33a8ce9f58f8768ec
Miguel Garcia <miggarcia@csumb.edu>
2023-07-14 15:38:31 -0700

    Javascript updated

--- Frontend/static/js/plots.js
+++ Frontend/static/js/plots.js
@@ -1,37 +1,73 @@
-const os = require('os');
-const { createEngine } = require('sqlalchemy');
-const pandas = require('pandas-js');
-const plotly = require('plotly.js-dist');
+// Function to create the Plotly bar graph
+function createBarGraph(data) {
+  const countyYearMap = {};
 
-const cwd = os.cwd();
-console.log(cwd);
+  // Process the data and count the wildfires per county and year
+  data.forEach(row => {
+    const county = row.COUNTY;
+    const year = row.FIRE_YEAR;
 
-const engine = createEngine('sqlite://Backend/WildfiresDB.db');
+    if (!countyYearMap[county]) {
+      countyYearMap[county] = {};
+    }
 
-const wildfires_data = pandas.read_sql_table('fire_data', engine);
+    if (!countyYearMap[county][year]) {
+      countyYearMap[county][year] = 0;
+    }
 
-const avg_wildfires_per_county = wildfires_data.groupby('COUNTY').size().mean();
-console.log('Average wildfires per county:', avg_wildfires_per_county);
+    countyYearMap[county][year]++;
+  });
 
-const most_severe_wildfires = wildfires_data[wildfires_data['FIRE_SIZE_CLASS'] === 'G'].sort_values('COUNTY');
+  const counties = Object.keys(countyYearMap);
+  const years = [...new Set(data.map(row => row.FIRE_YEAR))];
+  const numWildfires = [];
 
-const data = [
-  {
-    x: most_severe_wildfires['COUNTY'],
-    y: most_severe_wildfires['FIRE_SIZE'],
-    type: 'bar',
-    xaxis: 'x',
-    yaxis: 'y',
-    marker: { color: 'blue' },
-    hovertemplate: '%{x}: %{y}',
+  // Build the numWildfires array based on the county-year mapping
+  counties.forEach(county => {
+    const wildfiresPerYear = [];
+    years.forEach(year => {
+      const count = countyYearMap[county][year] || 0;
+      wildfiresPerYear.push(count);
+    });
+    numWildfires.push(wildfiresPerYear);
+  });
+
+  const plotData = [];
+  
+  for (let i = 0; i < years.length; i++) {
+    const bar = {
+      x: counties,
+      y: numWildfires.map(row => row[i]),
+      type: 'bar',
+      name: years[i].toString()
+    };
+    
+    plotData.push(bar);
   }
-];
-const layout = {
-  xaxis: { title: 'County' },
-  yaxis: { title: 'Fire Size' },
-  title: 'Most Severe Wildfires (Fire Size Class G)',
-};
-plotly.newPlot('chart', data, layout);
 
-const avg_acres_burned = most_severe_wildfires['FIRE_SIZE'].mean();
-console.log('Average acres burned for severe wildfires:', avg_acres_burned);
+  const layout = {
+    title: 'Number of Wildfires in Each County for Each Year',
+    xaxis: {
+      title: 'County'
+    },
+    yaxis: {
+      title: 'Number of Wildfires'
+    }
+  };
+
+  Plotly.newPlot('barGraph', plotData, layout);
+}
+
+// Fetch data from the API and create the bar graph
+fetch('http://127.0.0.1:5000/api/cawildfires17-20')
+  .then(response => response.json())
+  .then(data => {
+    createBarGraph(data);
+  })
+  .catch(error => console.error(error));
+
+const wildfireGeojson = '../../Data/OutputData/CaliWildfires.geojson';
+d3.json(wildfireGeojson).then(function(data) {
+  console.log(data);
+});
+
