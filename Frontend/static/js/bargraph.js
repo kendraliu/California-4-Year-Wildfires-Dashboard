da5659b5ca8860603d80cedf5095d033871bf077
daisy287 <drodriguez2874@gmail.com>
2023-07-14 17:06:38 -0700

    fixed html viewing

--- Frontend/static/js/bargraph.js
+++ Frontend/static/js/bargraph.js
@@ -1,38 +1,75 @@
 // plot.js
 
 // Function to create the Plotly bar graph
-function createBarGraph(years, numWildfires) {
-    var data = [{
-      x: years,
-      y: numWildfires,
-      type: 'bar'
-    }];
+function createBarGraph(data) {
+  var countyYearMap = {};
+
+  // Process the data and count the wildfires per county and year
+  data.forEach(row => {
+    var county = row.COUNTY;
+    var year = row.FIRE_YEAR;
+
+    if (!countyYearMap[county]) {
+      countyYearMap[county] = {};
+    }
+
+    if (!countyYearMap[county][year]) {
+      countyYearMap[county][year] = 0;
+    }
+
+    countyYearMap[county][year]++;
+  });
+
+  var counties = Object.keys(countyYearMap);
+  var years = [...new Set(data.map(row => row.FIRE_YEAR))];
+  var numWildfires = [];
+
+  // Build the numWildfires array based on the county-year mapping
+  counties.forEach(county => {
+    var wildfiresPerYear = [];
+    years.forEach(year => {
+      var count = countyYearMap[county][year] || 0;
+      wildfiresPerYear.push(count);
+    });
+    numWildfires.push(wildfiresPerYear);
+  });
+
+  var data = [];
   
-    var layout = {
-      title: 'Number of Wildfires in Each County for Each Year',
-      xaxis: {
-        title: 'Year'
-      },
-      yaxis: {
-        title: 'Number of Wildfires'
-      }
+  for (var i = 0; i < years.length; i++) {
+    var bar = {
+      x: counties,
+      y: numWildfires.map(row => row[i]),
+      type: 'bar',
+      name: years[i].toString()
     };
-  
-    Plotly.newPlot('barGraph', data, layout);
-  }
-  
-  // Fetch data from the API and create the bar graph
-  fetch('http://127.0.0.1:5000/api/cawildfires17-20')
-    .then(response => response.json())
-    .then(data => {
-      
-      var years = data.map(row => row.FIRE_YEAR);
-      var numWildfires = data.map(row => row.num_wildfires);
-      console.log(years)
-      console.log("this is num wildfires ")
-      console.log(numWildfires)
-      createBarGraph(years, numWildfires);
-    })
-    .catch(error => console.error(error));
-  
     
+    data.push(bar);
+  }
+
+  var layout = {
+    title: 'Number of Wildfires in Each County for Each Year',
+    xaxis: {
+      title: 'County'
+    },
+    yaxis: {
+      title: 'Number of Wildfires'
+    }
+  };
+
+  Plotly.newPlot('barGraph', data, layout);
+}
+
+// Fetch data from the API and create the bar graph
+fetch('http://127.0.0.1:5000/api/cawildfires17-20')
+  .then(response => response.json())
+  .then(data => {
+    createBarGraph(data);
+  })
+  .catch(error => console.error(error));
+
+    
+  
+  d3.json(wildfireGeojson).then(function(data){
+    console.log(data)
+  })
